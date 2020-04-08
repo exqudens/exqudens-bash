2f66f8760a7548a8406f59863d2e6ccaedf7c209
andrew <andrew.gorin@rocketbnk.com>
2020-04-08 23:04:26 +0300

    step 5

--- docker-run-postgres.sh
+++ docker-run-postgres.sh
@@ -2,10 +2,11 @@
 
 docker run \
 --rm \
---name postgres \
---publish=5432:5432 \
---env=POSTGRES_HOST_AUTH_METHOD=trust \
---env=POSTGRES_DB=test_db \
---env=POSTGRES_USER=test_user \
---env=POSTGRES_PASSWORD=test_pass \
---detach postgres:12.2
+--detach \
+--name 'postgres' \
+--publish '5432:5432' \
+--env 'POSTGRES_HOST_AUTH_METHOD=trust' \
+--env 'POSTGRES_DB=test_db' \
+--env 'POSTGRES_USER=test_user' \
+--env 'POSTGRES_PASSWORD=test_pass' \
+'postgres:12.2'
