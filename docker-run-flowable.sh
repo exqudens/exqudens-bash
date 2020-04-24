043e046882fc9710417b4d26f56e33014e8a5ba5
andrew <andrew.gorin@rocketbnk.com>
2020-04-24 14:32:19 +0300

    step 6

--- docker-run-flowable.sh
+++ docker-run-flowable.sh
@@ -6,4 +6,10 @@ docker run \
 --name 'flowable' \
 --publish '8080:8080' \
 --env 'SERVER_PORT=9977' \
-'flowable/all-in-one:6.5.0'
+'flowable/all-in-one:6.5.0' && \
+
+echo 'uri=http://localhost:8080/flowable-modeler' && \
+echo 'username=admin' && \
+echo 'password=test' && \
+
+exit 0
