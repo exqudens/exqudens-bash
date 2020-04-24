2f0ff1f352321ce4adcffedadd002c78b165e3fe
andrew <andrew.gorin@rocketbnk.com>
2020-04-24 16:44:34 +0300

    step 8

--- docker-run-openapi-generator-cli.sh
+++ docker-run-openapi-generator-cli.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Go-Args: '-i /local/petstore.yaml -g go -o /local/out/go'
-# Java-Args: '-g java --library okhttp-gson -i /local/Mastercard_Send_Disbursements-1.0.yaml -c /local/config.json -o /local/out/java_api_client'
+# Java-Args: '-g java --library okhttp-gson -i /local/Mastercard_Send_Disbursements-1.0.yaml -c /local/config.json -o /local/out/mastercard-disbursement-client'
 
 docker run \
 --rm \
