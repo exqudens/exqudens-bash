948e653c4922a7f0bf7f78c14af9c5a2eb724802
exqudens <exqudens@gmail.com>
2021-07-24 21:23:36 -0700

    step 13

--- other/msvc-paths.sh
+++ other/msvc-paths.sh
@@ -2,49 +2,62 @@
 
 set -e
 
-SCRIPT_CMD_COMMAND_PREFIX="/c vcvarsall.bat"
-SCRIPT_CMD_COMMAND_SUFFIX="&& call echo INCLUDE"
-SCRIPT_CMD_COMMAND_SUFFIX="${SCRIPT_CMD_COMMAND_SUFFIX} && call echo %INCLUDE%"
-SCRIPT_CMD_COMMAND_SUFFIX="${SCRIPT_CMD_COMMAND_SUFFIX} && call echo LIBPATH"
-SCRIPT_CMD_COMMAND_SUFFIX="${SCRIPT_CMD_COMMAND_SUFFIX} && call echo %LIBPATH%"
-SCRIPT_CMD_COMMAND_SUFFIX="${SCRIPT_CMD_COMMAND_SUFFIX} && call echo LIB"
-SCRIPT_CMD_COMMAND_SUFFIX="${SCRIPT_CMD_COMMAND_SUFFIX} && call echo %LIB%"
-SCRIPT_CMD_COMMAND_SUFFIX="${SCRIPT_CMD_COMMAND_SUFFIX} && call echo CLPATH"
-SCRIPT_CMD_COMMAND_SUFFIX="${SCRIPT_CMD_COMMAND_SUFFIX} && call where cl"
-SCRIPT_CMD_COMMAND_SUFFIX="${SCRIPT_CMD_COMMAND_SUFFIX} && call echo RCPATH"
-SCRIPT_CMD_COMMAND_SUFFIX="${SCRIPT_CMD_COMMAND_SUFFIX} && call where rc"
-SCRIPT_CMD_COMMAND_SUFFIX="${SCRIPT_CMD_COMMAND_SUFFIX} && call echo STOP"
+if test -f "tmp/msvc-paths.txt"
+then
+  SCRIPT_MSVC_ENV=$(cat "tmp/msvc-paths.txt")
+else
+  SCRIPT_MSVC_ENV=$(cmake -P "/c/git/exqudens-cmake/src/main/cmake/msvc-paths.cmake" 16 2>&1)
+  echo "${SCRIPT_MSVC_ENV}" > "tmp/msvc-paths.txt"
+fi
 
-SCRIPT_MSVC_INSTANCE_CMD_DIR_PATH=$('C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe' -latest -property installationPath)
-SCRIPT_MSVC_INSTANCE_BASH_DIR_PATH=$(readlink -f "${SCRIPT_MSVC_INSTANCE_CMD_DIR_PATH}")
-SCRIPT_MSVC_VCVARSALL_BASH_FILE_PATH=$(find "${SCRIPT_MSVC_INSTANCE_BASH_DIR_PATH}" -name 'vcvarsall.bat')
-SCRIPT_MSVC_VCVARSALL_BASH_DIR_PATH=$(dirname "${SCRIPT_MSVC_VCVARSALL_BASH_FILE_PATH}")
+SCRIPT_MSVC_X86_ENV=$(echo "${SCRIPT_MSVC_ENV}" | awk '/X86_X86_ENV_START/{flag=1;next}/X86_X86_ENV_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
 
-pushd "${SCRIPT_MSVC_VCVARSALL_BASH_DIR_PATH}"
+SCRIPT_MSVC_X86_LIBPATH=$(echo "${SCRIPT_MSVC_X86_ENV}" | awk '/LIBPATH_START/{flag=1;next}/LIBPATH_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
+SCRIPT_MSVC_X86_LIB=$(echo "${SCRIPT_MSVC_X86_ENV}" | awk '/LIB_START/{flag=1;next}/LIB_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
+SCRIPT_MSVC_X86_BASH_CLPATH=$(echo "${SCRIPT_MSVC_X86_ENV}" | awk '/BASH_CLPATH_START/{flag=1;next}/BASH_CLPATH_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
+SCRIPT_MSVC_X86_BASH_RCPATH=$(echo "${SCRIPT_MSVC_X86_ENV}" | awk '/BASH_RCPATH_START/{flag=1;next}/BASH_RCPATH_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
 
-SCRIPT_MSVC_X86_CMD_ENV=$(cmd "${SCRIPT_CMD_COMMAND_PREFIX} x86 ${SCRIPT_CMD_COMMAND_SUFFIX}")
+export MSVC_X86_LIBPATH="${SCRIPT_MSVC_X86_LIBPATH}"
+export MSVC_X86_LIB="${SCRIPT_MSVC_X86_LIB}"
+export MSVC_X86_BASH_CLPATH="${SCRIPT_MSVC_X86_BASH_CLPATH}"
+export MSVC_X86_BASH_RCPATH="${SCRIPT_MSVC_X86_BASH_RCPATH}"
 
-SCRIPT_MSVC_X64_X86_CMD_ENV=$(cmd "${SCRIPT_CMD_COMMAND_PREFIX} x64_x86 ${SCRIPT_CMD_COMMAND_SUFFIX}")
+SCRIPT_MSVC_X64_X86_ENV=$(echo "${SCRIPT_MSVC_ENV}" | awk '/X64_X86_ENV_START/{flag=1;next}/X64_X86_ENV_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
 
-SCRIPT_MSVC_X64_CMD_ENV=$(cmd "${SCRIPT_CMD_COMMAND_PREFIX} x64 ${SCRIPT_CMD_COMMAND_SUFFIX}")
+SCRIPT_MSVC_X64_X86_LIBPATH=$(echo "${SCRIPT_MSVC_X64_X86_ENV}" | awk '/LIBPATH_START/{flag=1;next}/LIBPATH_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
+SCRIPT_MSVC_X64_X86_LIB=$(echo "${SCRIPT_MSVC_X64_X86_ENV}" | awk '/LIB_START/{flag=1;next}/LIB_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
+SCRIPT_MSVC_X64_X86_BASH_CLPATH=$(echo "${SCRIPT_MSVC_X64_X86_ENV}" | awk '/BASH_CLPATH_START/{flag=1;next}/BASH_CLPATH_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
+SCRIPT_MSVC_X64_X86_BASH_RCPATH=$(echo "${SCRIPT_MSVC_X64_X86_ENV}" | awk '/BASH_RCPATH_START/{flag=1;next}/BASH_RCPATH_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
 
-SCRIPT_MSVC_X86_X64_CMD_ENV=$(cmd "${SCRIPT_CMD_COMMAND_PREFIX} x86_x64 ${SCRIPT_CMD_COMMAND_SUFFIX}")
+export MSVC_X64_X86_LIBPATH="${SCRIPT_MSVC_X64_X86_LIBPATH}"
+export MSVC_X64_X86_LIB="${SCRIPT_MSVC_X64_X86_LIB}"
+export MSVC_X64_X86_BASH_CLPATH="${SCRIPT_MSVC_X64_X86_BASH_CLPATH}"
+export MSVC_X64_X86_BASH_RCPATH="${SCRIPT_MSVC_X64_X86_BASH_RCPATH}"
 
-popd
+SCRIPT_MSVC_X64_ENV=$(echo "${SCRIPT_MSVC_ENV}" | awk '/X64_X64_ENV_START/{flag=1;next}/X64_X64_ENV_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
 
-echo '---'
-echo "${SCRIPT_MSVC_X86_CMD_ENV}"
-echo '---'
-echo "${SCRIPT_MSVC_X64_X86_CMD_ENV}"
-echo '---'
-echo "${SCRIPT_MSVC_X64_CMD_ENV}"
-echo '---'
-echo "${SCRIPT_MSVC_X86_X64_CMD_ENV}"
-echo '---'
-echo ''
+SCRIPT_MSVC_X64_LIBPATH=$(echo "${SCRIPT_MSVC_X64_ENV}" | awk '/LIBPATH_START/{flag=1;next}/LIBPATH_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
+SCRIPT_MSVC_X64_LIB=$(echo "${SCRIPT_MSVC_X64_ENV}" | awk '/LIB_START/{flag=1;next}/LIB_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
+SCRIPT_MSVC_X64_BASH_CLPATH=$(echo "${SCRIPT_MSVC_X64_ENV}" | awk '/BASH_CLPATH_START/{flag=1;next}/BASH_CLPATH_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
+SCRIPT_MSVC_X64_BASH_RCPATH=$(echo "${SCRIPT_MSVC_X64_ENV}" | awk '/BASH_RCPATH_START/{flag=1;next}/BASH_RCPATH_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
 
-SCRIPT_MSVC_X86_CMD_INCLUDE=$(echo "${SCRIPT_MSVC_X86_CMD_ENV}" | awk '/INCLUDE/{flag=1;next}/LIBPATH/{flag=0}flag')
+export MSVC_X64_LIBPATH="${SCRIPT_MSVC_X64_LIBPATH}"
+export MSVC_X64_LIB="${SCRIPT_MSVC_X64_LIB}"
+export MSVC_X64_BASH_CLPATH="${SCRIPT_MSVC_X64_BASH_CLPATH}"
+export MSVC_X64_BASH_RCPATH="${SCRIPT_MSVC_X64_BASH_RCPATH}"
 
-echo '==='
-echo "${SCRIPT_MSVC_X86_CMD_INCLUDE}"
-echo '==='
+SCRIPT_MSVC_X86_X64_ENV=$(echo "${SCRIPT_MSVC_ENV}" | awk '/X86_X64_ENV_START/{flag=1;next}/X86_X64_ENV_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
+
+SCRIPT_MSVC_X86_X64_LIBPATH=$(echo "${SCRIPT_MSVC_X86_X64_ENV}" | awk '/LIBPATH_START/{flag=1;next}/LIBPATH_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
+SCRIPT_MSVC_X86_X64_LIB=$(echo "${SCRIPT_MSVC_X86_X64_ENV}" | awk '/LIB_START/{flag=1;next}/LIB_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
+SCRIPT_MSVC_X86_X64_BASH_CLPATH=$(echo "${SCRIPT_MSVC_X86_X64_ENV}" | awk '/BASH_CLPATH_START/{flag=1;next}/BASH_CLPATH_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
+SCRIPT_MSVC_X86_X64_BASH_RCPATH=$(echo "${SCRIPT_MSVC_X86_X64_ENV}" | awk '/BASH_RCPATH_START/{flag=1;next}/BASH_RCPATH_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
+
+export MSVC_X86_X64_LIBPATH="${SCRIPT_MSVC_X86_X64_LIBPATH}"
+export MSVC_X86_X64_LIB="${SCRIPT_MSVC_X86_X64_LIB}"
+export MSVC_X86_X64_BASH_CLPATH="${SCRIPT_MSVC_X86_X64_BASH_CLPATH}"
+export MSVC_X86_X64_BASH_RCPATH="${SCRIPT_MSVC_X86_X64_BASH_RCPATH}"
+
+SCRIPT_MSVC_X86_X64_INCLUDE=$(echo "${SCRIPT_MSVC_X86_X64_ENV}" | awk '/INCLUDE_START/{flag=1;next}/INCLUDE_STOP/{flag=0}flag' | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }')
+
+export MSVC_INCLUDE="${SCRIPT_MSVC_X64_INCLUDE}"
